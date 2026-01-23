e732866368a9e0b0bfa088d10ccc6a84d0cc1e24
scbj <scbj@zhaw.ch>
2026-01-23 12:17:33 +0100

    (scripts/i3) keyboard layout layout switcher for i3 mostly working

--- scripts/i3-kb-layout-switcher.sh
+++ scripts/i3-kb-layout-switcher.sh
@@ -1,49 +1,55 @@
 #!/usr/bin/env bash
 
 function switch_kb_layout {
-    layouts=("us", "us")
-    variants=("default, intl")
-    languages="$(echo "${devices}" | grep -ioP '(?<="layout": ").*(?=")' -m 1)"
-    variants="$(echo "${devices}" | grep -ioP '(?<="variant": ").*(?=")' -m 1)"
+    layouts=("us" "us" "ch")
+    variants=("" "intl" "")
+    lines=${#layouts[@]}
+    number=0
+    selection=-1
 
-    ## TODO: use `setxkbmap -print` to get status
-    current="$(echo "${devices}" | grep -ioP '(?<="active_layout_index": ).*(?=,)' -m 1)"
+    if [[ ${#layouts[@]} -ne ${#variants[@]} ]]; then
+        notify-send "layout-switcher" "#layouts != #variants"
+        return 1
+    fi
 
-    languages=(${languages})
-    variants=(${variants})
-
-    number=$((${#languages[@]} - 1))
-    for n in $(seq 0 $number); do
-        layouts+=("${languages[$n]//,/} - ${variants[$n]//,/}")
+    unset names
+    for n in ${!layouts[@]}; do
+        if [[ -n "${variants[$n]}" ]]; then
+            names+=("${layouts[$n]}:${variants[$n]}")
+        else
+            names+=("${layouts[$n]}")
+        fi
     done
 
+    # ## TODO: use `setxkbmap -print` to get status
+    # current="$(echo "${devices}" | grep -ioP '(?<="active_layout_index": ).*(?=,)' -m 1)"
+
     if $(which rofi &>/dev/null); then
-        # selection="$(printf "%s\n" "${layouts[@]}" | fuzzel --lines ${#layouts[@]} --dmenu --select-index ${current})"
-        return 1
+        selection="$(printf "%s\n" "${names[@]}" | rofi -dmenu -format 'i' -fixed-num-lines ${lines} -selected-row ${number} -case-smart)"
     else
         notify-send "layout-switcher" "requires [rofi]"
         return 1
     fi
 
-    for n in $(seq 0 $number); do
-        if [[ "${layouts[$n]}" == "${selection}" ]]; then
-            index=$n
-            break
-        fi
-    done
-    if [[ -z "${index}" ]]; then
+    if [[ -z "$selection" ]]; then
+        return 0
+    fi
+
+    if [[ $selection -lt 0 || $selection -ge $lines ]]; then
+        notify-send "layout-switcher" "invalid selection"
         return 1
     fi
 
-    hyprctl switchxkblayout all "${index}" >/dev/null
+    # if [[ $selection -ge $lines ]]; then
+    #     notify-send "layout-switcher" "invalid selection"
+    #     return 1
+    # fi
+    #
+    if [[ -n "${variants[$selection]}" ]]; then
+        setxkbmap -layout "${layouts[$selection]}" -variant "${variants[$selection]}"
+    else
+        setxkbmap -layout "${layouts[$selection]}"
+    fi
 }
 
-# switch_kb_layout
-
-function choose_layout_us {
-    setxkbmap -layout us
-}
-
-function choose_layout_int {
-    setxkbmap -layout us -variant intl
-}
+switch_kb_layout
